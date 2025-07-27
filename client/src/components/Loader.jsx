34232a9265b8142f619387e5539b6582f7b95f11
Ojas Maheshwari <ctfbbonly@gmail.com>
2025-07-27 12:55:23 +0530

    Update

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,6 +4,7 @@ const Loader = () => {
     return (
         <div className='min-h-screen flex items-center justify-center flex-col'>
             <img src="https://cdn.dribbble.com/userupload/23237805/file/original-b01f81713802bdc20e9ce2ba275aec43.gif" alt="Loading..." className='w-64 h-64' />
+            <h1 className='text-center text-lg'>Please be patient.. We run on free servers :)</h1>
         </div>
     )
 }
