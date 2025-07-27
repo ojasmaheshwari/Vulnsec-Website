3c4ae153e0bc6ff69de6130f21cc6d5892bf2d59
Ojas Maheshwari <ctfbbonly@gmail.com>
2025-07-27 09:49:18 +0530

    update

--- client/src/api_endpoints.js
+++ client/src/api_endpoints.js
@@ -1,4 +1,4 @@
 
 export const ENVIRONMENT = "PROD";
-export const SERVER_URL = ENVIRONMENT === "DEV" ? "http://localhost:8080" : "http://34.63.113.174:8080";
+export const SERVER_URL = ENVIRONMENT === "DEV" ? "http://localhost:8080" : "https://34.63.113.174";
 export const CLIENT_URL = "https://vulnsec.netlify.app"
