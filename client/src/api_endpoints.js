5a20e226217abb11880b88fe5eee161977e753f9
root <root@free-linux-vm.us-central1-c.c.tokyo-amphora-344203.internal>
2025-07-27 06:25:32 +0000

    Update

--- client/src/api_endpoints.js
+++ client/src/api_endpoints.js
@@ -1,4 +1,3 @@
-
 export const ENVIRONMENT = "PROD";
 export const SERVER_URL = ENVIRONMENT === "DEV" ? "http://localhost:8080" : "https://vulnsec.ddns.net";
 export const CLIENT_URL = "https://vulnsec.netlify.app"
