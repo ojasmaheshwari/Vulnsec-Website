f44dad6409aaa2338931f08ec08c724dd1c8df68
ojasmaheshwari <ojasmaheshwari2006@gmail.com>
2025-10-20 09:59:53 +0530

    Switch to prod

--- client/src/api_endpoints.js
+++ client/src/api_endpoints.js
@@ -1,4 +1,4 @@
-export const ENVIRONMENT = "DEV"; // Change to "PROD" for production
+export const ENVIRONMENT = "PROD"; // Change to "PROD" for production
 export const SERVER_URL = ENVIRONMENT === "DEV" ? "https://localhost" : "https://vulnsec.ddns.net";
 export const CLIENT_URL = "https://vulnsec.netlify.app"
 
