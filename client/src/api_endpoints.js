6cbd17966e1f231bf87acf74a9194a928433c8c1
ojasmaheshwari <ojasmaheshwari2006@gmail.com>
2025-10-26 14:01:46 +0530

    update

--- client/src/api_endpoints.js
+++ client/src/api_endpoints.js
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = "PROD"; // Change to "PROD" for production
-export const SERVER_URL = ENVIRONMENT === "DEV" ? "https://localhost" : "https://vulnsec-website.onrender.com";
+export const SERVER_URL = ENVIRONMENT === "DEV" ? "https://localhost" : "https://vulnsec-server.onrender.com";
 export const CLIENT_URL = "https://vulnsec.netlify.app"
 
 export const CLOUDINARY_UPLOAD_URL = "https://api.cloudinary.com/v1_1/dfjnshwx5/upload";
